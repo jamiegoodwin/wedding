a4cd7756a8af99ef3178420bc539c25cfde3590f
Jamie Goodwin <me@jamiegoodwin.uk>
2019-05-07 09:53:29 +0200

    additional info, surge prepush

--- js/main.js
+++ js/main.js
@@ -14,22 +14,38 @@ function rsvp_send($data) {
 }
 
 function rsvp_form_update($status) {
+	let rsvp_form_container = document.querySelector('.rsvp-form-container');
+
+	// Create container
+	let container = document.createElement('div');
+	container.classList.add('rsvp-form-container');
+
+	// Container content (message)
 	let msg = document.createElement('p');
 	msg.classList.add('rsvp-msg');
 
-
-	if($status == 200) {
-		msg.innerHTML = 'It\'s all good!\n\nLooks like your RSVP\'s come through, but to be safe just double check your emails to make sure you have a copy. If not, email us manually on the address below.';
+	// Set message
+	if($status === 200) {
+		msg.innerHTML = 'It\'s all good!\n\nLooks like your RSVP\'s come through, but to be safe just double check your emails (including junk) to make sure you have a copy. If not, email us manually on the address below.';
 		msg.classList.add('success');
+	} else if ($status === 123) {
+		msg.innerHTML = '<picture><source srcset="img/sending.webp" type="image/webp"><source srcset="img/sending.gif" type="image/jpeg"><img src="img/sending.gif" /></picture>Sending by carrier pigeon...';
 	} else {
 		msg.innerHTML = 'Oops! Something went wrong. Could you do us a favour and email us manually on the address below to let us know you\'re coming?';
 		msg.classList.add('fail');
 	}
 
-	rsvp_form.parentElement.replaceChild(msg, rsvp_form);
+	// Insert msg into container
+	container.appendChild(msg);
+
+	// Update form message
+	rsvp_form_container.parentElement.replaceChild(container, rsvp_form_container);
 }
 
 rsvp_form.onsubmit = function () {
+	// Update form with sending message
+	rsvp_form_update(123);
+
 	let object = {};
 	let formData = new FormData(rsvp_form);
 
